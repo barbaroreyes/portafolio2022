4be5e1431e155cb9ba207e8ad59d7ec5d1dc8402
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-02-03 15:45:54 -0600

    change css

--- src/conponents/Picture.js
+++ src/conponents/Picture.js
@@ -2,16 +2,8 @@ import React from 'react';
 
 const Picture = () => {
   return (
-    <div style ={{
-        
-        width:"18%" 
-        ,height:"80px",
-        borderRadius:"50%" ,
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center"
-        }}>
-      <img  style={{width:"100%",borderRadius:"50%" ,objectFit:"fill",height:"80px"}}src ="https://avatars.githubusercontent.com/u/48423441?v=4" alt=""/>
+    <div className='picture' >
+      <img src ="https://avatars.githubusercontent.com/u/48423441?v=4" alt=""/>
     </div>
   );
 }
